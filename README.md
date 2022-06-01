066ac93a787cab2334e1a39e71af65f2068bbf71
Lauren McCarthy <laurenleemccarthy@gmail.com>
2022-06-01 16:04:55 -0700

    init

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## References
+* [Mapbox JS docs](https://github.com/mapbox/mapbox-sdk-js/blob/main/docs/services.md)
+* [Mapbox directions docs](https://docs.mapbox.com/api/navigation/directions/)
 * [Mapbox turn by turn navigation ref](https://www.mapbox.com/use-cases/turn-by-turn-navigation)
-* [JS docs](https://github.com/mapbox/mapbox-sdk-js/blob/main/docs/services.md#getdirections)
 * [Mapbox access tokens](https://account.mapbox.com/access-tokens)
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let init = false;
 let userLoc;
-let lastDir, dirInterval;
+let lastDir, dirInterval, route;
 
 $('#start').click(initApp);
 
@@ -8,9 +8,15 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibGF1cmVubGVlbWFjayIsImEiOiJja3BjMWJmMDcwNzh3M
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-122.662323, 45.523751], // starting position
-  zoom: 3 // starting zoom
+  center: [-118.441429, 34.076236],
+  zoom: 15
 });
+
+//temp
+// map.on('load', () => {
+//   userLoc = [-118.441429, 34.076236];
+//   initMap();
+// });
 
 map.on('load', () => {
   const geolocate = new mapboxgl.GeolocateControl({
@@ -102,28 +108,25 @@ function displayRoute(event) {
         }
       });
     }
-    if (dirInterval) clearInterval(dirInterval);
-    dirInterval = setInterval( () => { getRoute(coords); }, 1000);
+    getRoute(coords);
 }
 
 async function getRoute(end) {
   const query = await fetch(
-    `https://api.mapbox.com/directions/v5/mapbox/cycling/${userLoc[0]},${userLoc[1]};${end[0]},${end[1]}?steps=true&voice_instructions=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+    `https://api.mapbox.com/directions/v5/mapbox/driving/${userLoc[0]},${userLoc[1]};${end[0]},${end[1]}?steps=true&voice_instructions=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
     { method: 'GET' }
   );
   const json = await query.json();
   console.log(json)
-  const data = json.routes[0];
-  
-  updateDirections(data);
+  route = json.routes[0];
 
-  const route = data.geometry.coordinates;
+  const coords = route.geometry.coordinates;
   const geojson = {
     type: 'Feature',
     properties: {},
     geometry: {
       type: 'LineString',
-      coordinates: route
+      coordinates: coords
     }
   };
   // if the route already exists on the map, we'll reset it using setData
@@ -152,23 +155,93 @@ async function getRoute(end) {
   }
   // get the sidebar and add the instructions
   const instructions = document.getElementById('instructions');
-  const steps = data.legs[0].steps;
+  const steps = route.legs[0].steps;
 
   let tripInstructions = '';
   for (const step of steps) {
     tripInstructions += `<li>${step.maneuver.instruction}</li>`;
   }
   instructions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
-    data.duration / 60
+    route.duration / 60
   )} min 🚴 </strong></p><ol>${tripInstructions}</ol>`;
+
+
+  // if (dirInterval) clearInterval(dirInterval);
+  // dirInterval = setInterval( () => { checkRoute(); }, 1000);
+  checkRoute();
 }
 
-function updateDirections(data) {
-  let nextDir = data.legs[0].steps[0].voiceInstructions[0].announcement;
-  if (nextDir !== lastDir) {
-    console.log(nextDir);
-    let utter = new SpeechSynthesisUtterance(nextDir);
+function checkRoute() {
+  let step = getCurrentStep();
+  console.log(step);
+
+  let dir = getCurrentDirection(step);
+
+  if (dir.announcement !== lastDir) {
+    lastDir = dir.announcement;
+    let utter = new SpeechSynthesisUtterance(lastDir);
     window.speechSynthesis.speak(utter);
-    lastDir = nextDir;
+    console.log(lastDir);
   }
+}
+
+function getCurrentDirection(step) {
+  let directions = route.legs[0].steps[step.curStep].voiceInstructions;
+  let minDist = 999999999999999999999;
+  let curDir = -1;
+
+  for (let i = 0; i < directions.length; i++) {
+    let dist = Math.abs(step.stepEndDist - directions[i].distanceAlongGeometry);
+    if (dist < minDist) {
+      minDist = dist;
+      curDir = i;
+    } else {
+      return directions[curDir];
+    }
+  }
+}
+
+function getCurrentStep() {
+  let minDist = 999999999999999999999;
+  let stepEndDist = 0;
+  let curStep = -1, curGeo = -1;
+  for (let j = 0; j < route.legs[0].steps.length; j++) {
+    let step = route.legs[0].steps[j];
+    // console.log('step ', j);
+    for (let i = step.geometry.coordinates.length - 1; i >= 0; i--) {
+      let geo = step.geometry.coordinates[i];
+      console.log('geo ', i);
+      let dist = getDistanceFromLatLonInM(userLoc[0], userLoc[1], geo[0], geo[1]);
+
+      console.log(j, i, geo[0], geo[1], dist);
+      if (dist < minDist) {
+        // console.log(dist, minDist, stepEndDist, curStep); 
+        if (i === step.geometry.coordinates.length - 1) stepEndDist = dist;
+        minDist = dist;
+        curStep = j;
+        curGeo = i;
+      } else {
+        console.log('curStep', curStep, 'curGeo', curGeo, 'endDist', stepEndDist); 
+        return {'curStep': curStep, 'curGeo': curGeo, 'stepEndDist': stepEndDist};
+      }
+    }
+  }
+}
+
+function getDistanceFromLatLonInM(lon1, lat1, lon2, lat2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c * 1000; // Distance in m
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
